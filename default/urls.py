df7443afa228c1f3ff381a5bf8fb21208e672aab
admin <asd190tw@gmail.com>
2019-04-10 16:08:29 +0800

    檢視主題投票

--- default/urls.py
+++ default/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('poll/', poll_list),
-    path('poll1/', PollList.as_view())
+    #path('poll0/', poll_list),
+    path('poll/', PollList.as_view()),
+    path('poll/<int:pk>/', PollDetail.as_view())
 ]
--- default/views.py
+++ default/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render_to_response
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import *
 
 # Create your views here.
-def poll_list(req):
-    polls =poll.objects.all()
-    return render_to_response('poll_list.html', {'items':polls})
+#def poll_list(req):
+    #polls =poll.objects.all()
+    #return render_to_response('poll_list.html', {'items':polls})
 
 class PollList(ListView):
-    model= Poll
+    model= poll
+
+    class PollDetail(DetailView):
+        model = Poll
