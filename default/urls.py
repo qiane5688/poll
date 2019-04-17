209fdfb3435a575759ea1c9367c36f1862b882a7
admin <asd190tw@gmail.com>
2019-04-17 15:39:49 +0800

    投票功能完成

--- default/urls.py
+++ default/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     #path('poll0/', poll_list),
     path('poll/', PollList.as_view()),
-    path('poll/<int:pk>/', PollDetail.as_view())
+    path('poll/<int:pk>/', PollDetail.as_view()),
+    path('vote/<int:oid>', PollVote.as_view()),
 ]
--- default/views.py
+++ default/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render_to_response
-from django.views.generic import ListView, DetailView
+from django.views.generic import *
 from .models import *
 
 # Create your views here.
@@ -10,5 +10,17 @@ from .models import *
 class PollList(ListView):
     model= poll
 
-    class PollDetail(DetailView):
-        model = Poll
+class PollDetail(DetailView):
+    model = poll
+
+    def get_context_data(self, **kwargs):
+        cxt = super().get_context_data(**kwargs)
+        ctx['options'] = Option.objects.filter(poll_id=self.kwargs['pk']) 
+        return cxt
+
+class PollVote(RedirectView): 
+    def get_redirect_url(self, *args, **kwargs):
+        item = Option.objects.get(id=self.kwargs['oid'])
+        item.count += 1
+        item.save()
+        return '/poll/'+str(item.poll_id)+'/' 
